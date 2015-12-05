5fb18c6d1f1b1bf6865cbdd39c64064e45712238
Melody Sue <msue16@cmc.edu>
2015-12-05 10:22:17 -0800

    added Library view controller and fixed Instructions and Library segues

---
+++ APP/HMC Word Scroll/View Controllers/LibraryViewController.swift
@@ -0,0 +1,24 @@
+//
+//  LibraryViewController.swift
+//  HMC Word Scroll
+//
+//  Created by Melody Sue on 12/5/15.
+//  Copyright © 2015 HMC LAPDOG. All rights reserved.
+//
+
+import UIKit
+
+class LibraryViewController: UIViewController {
+    
+    //UI Elements and defaults
+    
+    
+    override func viewDidLoad() {
+        
+        
+        
+    }
+    
+    
+    
+}
